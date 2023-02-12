bcde8ee6daacc2ede38515adc86e0d291425b647
MatthewWillenbring <matthewwillenbring@gmail.com>
2023-02-12 16:30:57 -0600

    all sites

--- website/urls.py
+++ website/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('', views.index, name="index"),
     path('index.html', views.index, name="index"),
     path('contact.html', views.contact, name="contact"),
     path('about.html', views.about, name="about"),
